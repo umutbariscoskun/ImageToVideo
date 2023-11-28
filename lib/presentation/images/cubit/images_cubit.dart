4343cb0d5f47829d6f3acfaea8d0d4fc7d5629bf
umutbariscoskun <umutcoskun2017@gmail.com>
2023-11-28 21:23:08 +0300

    add image picker logics

--- lib/presentation/images/cubit/images_cubit.dart
+++ lib/presentation/images/cubit/images_cubit.dart
@@ -1,5 +1,6 @@
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'package:image_picker/image_picker.dart';
 
 import 'package:injectable/injectable.dart';
 
@@ -10,5 +11,37 @@ class ImagesCubit extends Cubit<ImagesState> {
   ImagesCubit() : super(const ImagesState()) {
     init();
   }
-  Future<void> init() async {}
+  late final ImagePicker _imagePicker;
+
+  Future<void> init() async {
+    _imagePicker = ImagePicker();
+  }
+
+  Future<void> pickMultipleImages() async {
+    List<XFile>? selectedImages = await _imagePicker.pickMultiImage();
+    if (selectedImages.isNotEmpty) {}
+    final allFiles = <XFile>[];
+    allFiles.addAll(state.imageFileList);
+    allFiles.addAll(selectedImages);
+    emit(state.copyWith(imageFileList: allFiles));
+  }
+
+  void updateSelectedList({required String path}) {
+    final selectedFiles = <String>[];
+    selectedFiles.addAll(state.selectedImageFileList);
+
+    if (selectedFiles.contains(path)) {
+      selectedFiles.removeWhere((element) => element == path);
+
+      emit(state.copyWith(selectedImageFileList: selectedFiles));
+    } else {
+      selectedFiles.add(path);
+
+      emit(state.copyWith(selectedImageFileList: selectedFiles));
+    }
+  }
+
+  bool checkForSelectedIcon({required String path}) {
+    return state.selectedImageFileList.contains(path);
+  }
 }
--- lib/presentation/images/images_view.dart
+++ lib/presentation/images/images_view.dart
@@ -1,5 +1,9 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:gap/gap.dart';
 import 'package:image_to_video/core/di/injectable.dart';
 import 'package:image_to_video/presentation/images/cubit/images_cubit.dart';
 
@@ -16,7 +20,73 @@ class ImagesView extends StatelessWidget {
             builder: (context, state) {
               final cubit = context.read<ImagesCubit>();
 
-              return Container();
+              return Column(
+                children: [
+                  Expanded(
+                      child: Padding(
+                    padding: EdgeInsets.all(20),
+                    child: GridView.builder(
+                      itemCount: state.imageFileList.length,
+                      gridDelegate:
+                          const SliverGridDelegateWithFixedCrossAxisCount(
+                        childAspectRatio: 1,
+                        crossAxisCount: 3,
+                        mainAxisSpacing: 5,
+                        crossAxisSpacing: 5,
+                      ),
+                      itemBuilder: (context, index) {
+                        return GestureDetector(
+                          onTap: () => cubit.updateSelectedList(
+                              path: state.imageFileList[index].path),
+                          child: Stack(
+                            alignment: Alignment.topRight,
+                            children: [
+                              SizedBox(
+                                width: 150.w,
+                                height: 150.h,
+                                child: Image.file(
+                                  File(
+                                    state.imageFileList[index].path,
+                                  ),
+                                  fit: BoxFit.cover,
+                                ),
+                              ),
+                              if (cubit.checkForSelectedIcon(
+                                  path: state.imageFileList[index].path))
+                                const Icon(
+                                  Icons.check,
+                                  color: Colors.white,
+                                )
+                            ],
+                          ),
+                        );
+                      },
+                    ),
+                  )),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      GestureDetector(
+                        onTap: () async => await cubit.pickMultipleImages(),
+                        child: Container(
+                          width: 150,
+                          height: 50,
+                          color: Colors.red,
+                        ),
+                      ),
+                      Gap(10.w),
+                      GestureDetector(
+                        onTap: () {},
+                        child: Container(
+                          width: 150,
+                          height: 50,
+                          color: Colors.blue,
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              );
             },
           ),
         ),
